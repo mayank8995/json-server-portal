e87b9fbde5a6b8d4f53e76a3222bfdd9440f1126
Mayank Gupta <mayankgupta8995@gmail.com>
2026-06-16 20:59:09 +0530

    cors

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ const middlewares = jsonServer.defaults()
 const allowedOrigins = ['https://advance-dashboard.onrender.com'];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
+  // origin: (origin, callback) => {
+  //   // Allow requests with no origin (like mobile apps or curl)
+  //   if (!origin) return callback(null, true);
     
-    if (allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV !== 'production') {
-      callback(null, true);
-    } else {
-      callback(new Error('Blocked by CORS policy'));
-    }
-  },
+  //   if (allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV !== 'production') {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Blocked by CORS policy'));
+  //   }
+  // },
+  origin:'https://advance-dashboard.onrender.com',
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization,X-Requested-With,Accept,Access-Control-Allow-Origin'
@@ -25,9 +26,9 @@ const corsOptions = {
 // Apply standard defaults (logger, static, read-only if configured)
 server.use(middlewares);
 // Attach the main json-server router
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1'  // optional: prefix all routes with /api
-}))
+// server.use(jsonServer.rewriter({
+//   '/api/*': '/$1'  // optional: prefix all routes with /api
+// }))
 
 
 // Enable robust CORS handling
