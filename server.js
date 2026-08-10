916e8bde2314051e8a4459ef0d6a8b83e9228023
mayank8995 <mayankgupta8995@gmail.com>
2026-08-10 17:57:48 +0530

    server js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(cookieParser());
 const appRoutes = require('./src/routes/appRoutes');
 
 const corsOptions = {
-  // origin: 'https://advance-dashboard.onrender.com',
-  origin: 'http://localhost:5173',
+  origin: 'https://advance-dashboard.onrender.com',
+  // origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
